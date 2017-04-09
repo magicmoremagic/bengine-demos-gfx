88cf415cb415ea16d1f35966cd3a00f2facb034b
Ben Crist <ben@magicmoremagic.com>
2017-04-08 19:14:53 -0500

    initial commit

---
+++ build.lua
@@ -0,0 +1,12 @@
+demo 'd-gfx' {
+   app {
+      icon 'icon/bengine.ico',
+      src 'src/tex.cpp',
+      link_project {
+          'gfx-tex',
+          'gfx',
+          'platform',
+          'core-id'
+      }
+   }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+## bengine `gfx` Demos
+For instructions on building, see the main repository at
+[magicmoremagic/bengine](https://github.com/magicmoremagic/bengine).
